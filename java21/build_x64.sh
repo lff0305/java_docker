6a4e1d6a98aa6de7cfecc5dcf6ba8ff2a6cbaf9d
lff0305 <lff0305@gmail.com>
2025-07-31 14:28:34 +0800

    No issue. Add scripts for jdk21

---
+++ java21/build_x64.sh
@@ -0,0 +1,18 @@
+set -e
+rm -rf ./jdk-*
+JDK_VER=21.0.8_9
+wget --no-check-certificate \
+     https://github.com/adoptium/temurin21-binaries/releases/download/jdk-`echo $JDK_VER | sed 's/_/%2B/g'`/OpenJDK21U-jdk_x64_linux_hotspot_${JDK_VER}.tar.gz
+tar xzvf OpenJDK21U-jdk_x64_linux_hotspot_${JDK_VER}.tar.gz
+rm -rf OpenJDK21U-jdk_x64_linux_hotspot_${JDK_VER}.tar.gz
+m=$(tr '\n' ',' < modules ) 
+rm -rf ./openjdk
+./jdk-`echo $JDK_VER | sed 's/_/+/g'`/bin/jlink --output ./openjdk \
+    --no-man-pages \
+    --no-header-files \
+    --compress=2 \
+    --add-modules $m
+ID=$(docker build . -q)
+docker run --rm $ID java -version
+echo "Docker image ID is $ID"
+echo "Please run docker tag $ID <Your Image>:<Your Tag> for later usage."
